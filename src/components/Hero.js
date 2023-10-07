9b4ff9f3e48a22fe7119dae2884d04225b24a916
Sbosh 11 <sbosh11@gmail.com>
2023-10-07 12:12:07 +0200

    mobile responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './Hero.css'; 
+import React from "react";
+import "./Hero.css";
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
-       <div className="hero-text">
-      <h1 className="hero-heading">Live with Confidence</h1>
-      <p className="hero-subtitle">José Mourinho brings confidence to pan-African Sanlam campaign.</p>
-      <button className="hero-button">View project</button>
+      <div className=" hero-text">
+        <h1 className="hero-heading">Live with Confidence</h1>
+        <p className="hero-subtitle">
+          José Mourinho brings confidence to pan-African Sanlam campaign.
+        </p>
+        <button className="hero-button">View project</button>
       </div>
     </div>
   );
